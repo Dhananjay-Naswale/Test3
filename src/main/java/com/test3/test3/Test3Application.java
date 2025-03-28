590331d60e18fdebdec9a64df6b19afd70ddaedd
Dhananjay-Naswale <66205073+Dhananjay-Naswale@users.noreply.github.com>
2025-03-28 20:32:18 +0530

    Update Test3Application.java

--- src/main/java/com/test3/test3/Test3Application.java
+++ src/main/java/com/test3/test3/Test3Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test3Application {
-	gvnbhmgm
-	hjhjhj
-	dhananjay 
-	hjsdhjsdj
 	public static void main(String[] args) {
+		String name = "mike";
+		int x = 100;
+		int y = 200;
+		double z = 10.3;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
